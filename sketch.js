658f9a2a3fb852c1d523abac23b84dc92b192fee
Inácio Aráujo <walkerf008@gmail.com>
2024-05-27 22:58:42 -0300

    1.2

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
-const SPACESHIP_VELOCYT = 4;
+const SPACESHIP_VELOCITY = 7;
+const BACKGROUND_VELOCITY = 20;
 
 let spaceship;
+let bgImage;
+let bgimageX = 0;
 let positionSpaceshipX = 100;
 let positionSpaceshipY = 300;
 
 function preload() {
+  bgImage = loadImage("./assets/Stars.jpeg");
   spaceship = loadImage("./assets/player_plane.png");
 }
 
@@ -15,15 +19,14 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-  image(spaceship, positionSpaceshipX, positionSpaceshipY, 100, 100);
-  if (keyCode === UP_ARROW) {
-    positionSpaceshipY -= SPACESHIP_VELOCYT;
-  } else if (keyCode === DOWN_ARROW) {
-    positionSpaceshipY += SPACESHIP_VELOCYT;
-  }
-}
+  image(bgImage, bgimageX % width, 0, width, height);
+  image(bgImage, (bgimageX % width) + width, 0, width, height);
 
-function keyReleased() {
-  keyCode = -1;
+  image(spaceship, positionSpaceshipX, positionSpaceshipY, 100, 100);
+  if (keyIsDown(UP_ARROW) || keyIsDown(87)) {
+    positionSpaceshipY -= SPACESHIP_VELOCITY;
+  } else if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) {
+    positionSpaceshipY += SPACESHIP_VELOCITY;
+  }
+  bgimageX -= BACKGROUND_VELOCITY;
 }
